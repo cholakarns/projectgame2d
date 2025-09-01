c67b4d284df1540e30e190f39fbb4a6d977fc3b6
Pacharapol <pacharapol.b@kkumail.com>
2025-09-02 01:56:51 +0700

    Add files via upload

---
+++ Scenes/Levels/level_01.gd
@@ -0,0 +1,12 @@
+extends Node2D
+@onready var player = $Player
+var die_count = 0
+func _ready() -> void:
+	GameManager.score = 0
+func _process(delta: float) -> void:
+	$UserInterface/GameUI/ProgressBar.value = $Player.get_health()
+	if($Trap/Spear_Trap.status):
+		die_count += 1
+	if die_count >= 1:
+		$TutorialText/SetNextScene3.visible = true
+		$TutorialText/SetNextScene2.visible = false
